e94af4fd80e199dd016fe916752dd21c45cb530a
samuelmartin <samuelmartin991@gmail.com>
2025-10-21 20:01:08 +0200

    index

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Descodificador Completo (Agente 0069)</title>
+  <link rel="stylesheet" href="css/styles.css">
+    <script src="js/descodificaciones.js"></script>
+
+</head>
+<body>
+
+  <h1>Descodificador de Mensajes Cifrados</h1>
+
+  <section>
+    <div class="botones">
+    <input type="text" id="mensaje" placeholder='Introduce el mensaje cifrado (X")'>
+      <button onclick="descodificar1()">Paso 1 </button>
+      <br> <br>
+      <button onclick="descodificar2()">Paso 2 </button>
+      <br>  <br>
+      <button onclick="descifrarCompleto()">Ultimo Paso</button>
+    </div>
+
+    <div id="resultado">
+
+    </div>
+  </section>
+
+</body>
+</html>
